c91efc7171374c70bd62d0decec182ffdab6fee7
peterheemskerk <peter.heemskerk@planet.nl>
2019-04-10 18:42:36 +0200

    ass1 ICP

--- Assignment_1/main.m
+++ Assignment_1/main.m
@@ -1,115 +1,162 @@
-%% Testing readPcd.m 
+%% Testing readPcd.m and reading source and target data
+
 clear all
 clc
 close all
-
-%{
-BaseCloud = readPcd('0000000000.pcd');
-TargetCloud = readPcd('0000000001.pcd');
-%}
-
-%% ICP
-
-% Initilize 3 by 3 identity matrix
-
-% Create function to search for all points in the base cloud to find the 
-% closest point in the target cloud. 
-% Will call this psi.
-% Need to output a matrix of pairwise combinations (base point, nearest target point)
-
-% THEN update approach, to get R and t using SVD.
-% create if else statements to see when RMS changes
-
-% just to try out some code and get used to the data
 
 % this is path where readPcd code is
 % readPcd code is amended: '\n' --> \r' Windows --> Linux
 addpath('./SupplementalCode') 
 % please note...    ./ is current node
+datapath = '../../data/';
 
-datapath = '../../data/'
+s = load('source.mat');
+size(s.source);
+t = load('target.mat');
+size(t.target);
 
-s = load('source.mat')
-s
-size(s.source)
+%{
+BaseCloud = readPcd([datapath, '0000000013_normal.pcd']);
+TargetCloud = readPcd([datapath, '0000000000.pcd']);
+%}
 
-t = load('target.mat')
-size(t.target)
+%% ICP
 
-a1 = readPcd([datapath, '0000000013_normal.pcd']);
-a2 = readPcd([datapath, '0000000000.pcd']);
+selectionType = 1
+nr_samples = 20         %only used for selectionType = 2 or 3
+maxIterations = 100;
+diffRMS = 0.0005;       % convergence if small improvement in RMS
 
-%% ICP - see below for main functions - I have to finilize ICP loop
-
-[psi, psi_source] = det_matching(s.source', t.target');
-[R, tr] = det_rotation(s.source', t.target', psi)
-RMS = calc_RMS(s.source', t.target', psi)
+[RMS, message, Rot, trans] = detICP(s.source', t.target', selectionType, nr_samples, maxIterations, diffRMS)
 
 %====================================================
 %  helper function
 %====================================================
 
-function [R, t] = det_rotation(source, target, psi)
-    % determine R and t with SVD
-    % target is original set of points Nx3, psi give the corresponding 
-    % indices of target-points which are closest to source
-    N = length(psi)
-    p = source(1:N, :)
-    q = target(psi, :)
-    % step1 - determine weighted cetroids of p and q
-    % note: weighting w = 1 for all points
-    pc = sum(p, 1)/N
-    qc = sum(q, 1)/N;
-    % step2 - centered vectors -> matrices D x N
-    X = (p - pc)'
-    Y = (q - qc)'
-    % step3 - determine covariance matrix S = XWY'
-    W = eye(N)    % all weights are = 1
-    S = X*W*Y'
-    % step 4 - singular value decomposition
-    [U Sigma Vt] = svd(S)
-    V = Vt'
-    s = svd(S)
-    Nr = size(Sigma, 1)
-    rot = eye(Nr);
-    rot(Nr, Nr) = det(V*U')
-    R = V*rot*U'
-    % Step 5 - optimal translation
-    t = qc' - R*pc'
+function [RMS, message, R, t] = detICP(source, target, selectionType,  nr_samples, maxIterations, diffRMS)
+   
+    % Initilize R, tr, RMS
+    dim = 3;
+    R = eye(dim);
+    t = zeros(dim, 1);
+    sourceRotated = source;    % init: no rotation or translation
+    
+    % if selectionType == 2, then initialize sample indices
+    if selectionType == 2
+            disp('selectionType=2 - fixed nr of samples')
+            Nmax = size(source, 1);
+            N = nr_samples;   
+            sampleInd = randi(Nmax, N, 1);
+    else
+            sampleInd = [];
+    end
+    
+    ii = 1;
+    oldRMS = 1000000000;
+    
+    [~, psiT, ~, ~] = det_matching(source, target);    
+    RMS = calc_RMS(sourceRotated, psiT)  % this is our loss function to minimize
+     
+    while (ii < maxIterations & (oldRMS-RMS > diffRMS))
+        oldRMS = RMS;
+        [~, psiT, ~, ~ ] = det_matching(sourceRotated, target);
+        [R, t] = detRotation(sourceRotated, psiT, selectionType, sampleInd, nr_samples);
+        sourceRotated = (R * sourceRotated' + t)';
+        RMS = calc_RMS(sourceRotated, psiT)
+        ii = ii + 1;
+    end
+    if ii == maxIterations
+        message = 'maxIterations reached';
+    elseif oldRMS-RMS < diffRMS
+        message = ['convergence in: ', num2str(ii),  ' steps'];
+    end 
 end
 
-function [psi, psi_source] = det_matching(source, target)
+function [R, t] = detRotation(source, psiTarget, selectionType, sampleInd, nr_samples)
+    % determine R and t with SVD
+    % source is the rotated source (N x 3)
+    % psiTarget is the psiTarget (so closed points attached)
+    %
+    % sampleInd only used if selectionType == 2
+    % nr_samples only used if selectionType == 3
+    switch selectionType
+        case 1    % all points
+            N = size(source, 1);
+            P = source;
+            Q = psiTarget;
+        case 2    % given sample of points
+            N = length(sampleInd);
+            P = source(sampleInd, :);
+            Q = psiTarget(sampleInd, :);
+        case 3    % every iteration new sample
+            Nmax = size(source, 1);
+            N = nr_samples;          % parameter: number of samples
+            sampleInd = randi(Nmax, N, 1);
+            P = source(sampleInd, :);
+            Q = psiTarget(sampleInd, :);
+        otherwise
+            disp('det_matching: this selectionType not supported!')
+    end
+    % step1 - determine weighted cetroids of p and q
+    % note: weighting w = 1 for all points
+    pc = sum(P, 1)/N;
+    qc = sum(Q, 1)/N;
+    % step2 - centered vectors -> matrices D x N
+    X = (P - pc)';
+    Xsum = sum(X, 2);   % check: should be 'zeroes'
+    Y = (Q - qc)';
+    Ysum = sum(Y, 2);   % check: should be 'zeroes'
+    % step3 - determine covariance matrix S = XWY'
+    W = eye(N);    % all weights are = 1
+    S = X*W*Y';
+    % step 4 - singular value decomposition
+    [U Sigma V] = svd(S);
+    s = svd(S);
+    Nr = size(Sigma, 1);
+    rot = eye(Nr);
+    rot(Nr, Nr) = det(V*U');
+    R = V*rot*U';
+    % Step 5 - optimal translation
+    t = qc' - R*pc';
+end
+
+function [psi, psiTarget, sampledSource, psiDistances] = det_matching(source, target)
     % source should be N x 3 matrix
     % target should be M x 3 matrix
     % psi is indices on target to map on source
-    % N = size(source, 1)
-    N = 5
+    sampledSource = [];
     psi = [];
-    psi_source = [];
+    psiTarget = [];
+    psiDistances = [];
+    N = size(source, 1);
+    % N = 5   % for testing
     for ii = 1:N
         point = source(ii, :);
-        [closest_index, closest_point] = find_closest_point(point, target);
-        psi_source = cat(1, psi_source, closest_point);
-        psi = cat(1, psi, closest_index);
+        [closestIndex, closestPoint, minDistance] = findClosestPoint(point, target);
+        sampledSource = cat(1, sampledSource, point);
+        psiTarget = cat(1, psiTarget, closestPoint);
+        psi = cat(1, psi, closestIndex);
+        psiDistances = cat(1, psiDistances, minDistance);
     end
 end
 
-function [closest_index, closest_point] = find_closest_point(point, target)
+function [closestIndex, closestPoint, minDistance] = findClosestPoint(point, target)
     % point is 1 x 3
     % target is N x 3
     % closest_point is 1 x 3
     % used brute force to determine the closest_point in target which
     % is closest to point. 
     distances = pdist2(target, point);
-    closest_index = find(distances == min(distances));
-    closest_point = target(closest_index, :);
+    minDistance = min(distances);
+    closestIndex = find(distances == minDistance);
+    closestPoint = target(closestIndex, :);
 end
 
-function [RMS] = calc_RMS (source, target, psi)
+function [RMS] = calc_RMS (source, target)
     % RMS distance calculated between source and psi(target)
     % target is original set of points, psi give the corresponding 
     % indices of target-points which are closest to source
-    N = size(psi, 1);
-    norms = sqrt(sum((source(1:N, :) - target(psi)).^2,2));
+    N = size(source, 1);
+    norms = sqrt(sum((source - target).^2,2));
     RMS = sqrt(sum(norms, 1)/N);
 end
