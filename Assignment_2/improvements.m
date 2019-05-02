0eb439472684798019d7ceba8459263fd89a927b
Guido <guido.v@bit-students.com>
2019-05-02 12:47:38 +0200

    started with improvements.m

---
+++ Assignment_2/improvements.m
@@ -0,0 +1,17 @@
+%{
+1. PVM density improvements:
+    This only works if you can somehow interpolate new points. Just
+    recapturingthe the feature chains doesn't improve the selection of
+    dense blocks. 
+
+2. Affine ambiguity elimination
+    Eliminating Affine ambiguity makes the point cloud reconstruction more
+    reliable.
+
+3. Improving the key-point matching
+    By improving the key-point matching the features will be picked more
+    reliably. This will translate into a denser point view matrix and a
+    better 3D point reconstruction.
+
+4. 
+}%
