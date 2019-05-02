f53738ba58ef659a7cb8c1f52fa2174108e819b4
Guido <guido.v@bit-students.com>
2019-05-02 11:34:55 +0200

    added removal of excess padding and made chaining into a function

--- Assignment_2/chaining.m
+++ Assignment_2/chaining.m
@@ -1,61 +1,63 @@
-clear
-clc
-close all
+function PVM = chaining()
 
-%% load images
-images = load_images();
+    %% load images
+    images = load_images();
 
 
-%% initialize padded PVM
-PVM = -1 .* ones(2*size(images, 3), 4000);
-threshold = 4;
+    %% initialize padded PVM
+    PVM = -1 .* ones(2*size(images, 3), 4000);
+    threshold = 4;
 
-image1 = images(:,:,1);
-image2 = images(:,:,2);
-
-[matches, features1, features2] = keypoint_matching( ...
-                                image1, image2, threshold);
-
-first_row = remove_doubles([features1(1:2, matches(1, :)); features2(1:2, matches(2, :))]);
-
-for i = 1:size(first_row, 2)
-    for k = 1:4
-        PVM(k, i) = first_row(k, i);
-    end
-end
-
-max_index = i;
-
-
-%% add rows to PVM
-for i = 2:size(images, 3)-1
-    
-    i_current = 2*i;
-    i_next = 2*(i+1);
+    image1 = images(:,:,1);
+    image2 = images(:,:,2);
 
     [matches, features1, features2] = keypoint_matching( ...
-                                  images(:,:,i), images(:,:,i+1), threshold);
+                                    image1, image2, threshold);
 
-    next_row = remove_doubles([features1(1:2, matches(1, :)); features2(1:2, matches(2, :))]);
+    first_row = remove_doubles([features1(1:2, matches(1, :)); features2(1:2, matches(2, :))]);
 
-    buffer = [];
-
-    for j = 1:size(next_row, 2)
-
-        index = find_index(PVM(i_current-1:i_current, :), next_row(1:2, j));
-
-        if index < 1
-            buffer = [buffer, next_row(3:4, j)];
-        else
-            PVM(i_next-1:i_next, index) = next_row(3:4, j);
+    for i = 1:size(first_row, 2)
+        for k = 1:4
+            PVM(k, i) = first_row(k, i);
         end
     end
 
-    for k = 1:size(buffer, 2)
-        PVM(i_next-1:i_next, max_index+k) = buffer(1:2, k);
+    max_index = i;
+
+
+    %% add rows to PVM
+    for i = 2:size(images, 3)-1
+
+        i_current = 2*i;
+        i_next = 2*(i+1);
+
+        [matches, features1, features2] = keypoint_matching( ...
+                                      images(:,:,i), images(:,:,i+1), threshold);
+
+        next_row = remove_doubles([features1(1:2, matches(1, :)); features2(1:2, matches(2, :))]);
+
+        buffer = [];
+
+        for j = 1:size(next_row, 2)
+
+            index = find_index(PVM(i_current-1:i_current, :), next_row(1:2, j));
+
+            if index < 1
+                buffer = [buffer, next_row(3:4, j)];
+            else
+                PVM(i_next-1:i_next, index) = next_row(3:4, j);
+            end
+        end
+
+        for k = 1:size(buffer, 2)
+            PVM(i_next-1:i_next, max_index+k) = buffer(1:2, k);
+        end
+
+        max_index = max_index + size(buffer, 2);
     end
 
-    max_index = max_index + size(buffer, 2);
+    PVM = remove_padding(PVM);
+
 end
 
 %%
@@ -73,6 +75,8 @@ directory = pwd + "/Data/House/";
 end
 
 function PVM = remove_padding(PVM)
+    indices = any(PVM > -1); 
+    PVM = PVM(:, indices);
 end
 
 function index = find_index(array1, coords)
