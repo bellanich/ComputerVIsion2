f2a7d1eb277faa055939f2a0fc64980a8937917c
peterheemskerk <peter.heemskerk@planet.nl>
2019-05-08 12:35:58 +0200

    qu2

--- Assignment_3/mesh_to_png.py
+++ Assignment_3/mesh_to_png.py
@@ -2,69 +2,60 @@ import os
 import numpy as np
 import trimesh
 import pyrender
-
 import h5py
-
 from data_def import PCAModel, Mesh
 
-bfm = h5py.File("./Data/model2017-1_face12_nomouth.h5", 'r')
+def load_faces(alpha, sigma):
 
-# here we load shape/model/mean and convert 3N --> N x 3
+	bfm = h5py.File("./Data/model2017-1_face12_nomouth.h5", 'r')
 
-mean_shape = np.asarray(bfm['shape/model/mean'], dtype=np.float32).reshape((-1, 3))
-size_mean_shape = np.shape(mean_shape)
-print(size_mean_shape)
+	# here we load shape/model/mean and convert 3N --> N x 3
 
-# here we load the pcabasis and pcavar (reshape it into right tensors, take squareroot of sigma, matrix multiply it. 
+	mean_shape = np.asarray(bfm['shape/model/mean'], dtype=np.float32).reshape((-1, 3))
+	size_mean_shape = np.shape(mean_shape)
 
-pcaB_shape = np.asarray(bfm['shape/model/pcaBasis'], dtype=np.float32)
-nrDim = np.shape(pcaB_shape)[1]
-pcaB_shape = pcaB_shape.reshape(-1, 3, nrDim)
-pcaB_shape = pcaB_shape[:, :, :30]
-# print('pcaB_shape:', np.shape(pcaB_shape))
+	# here we load the pcabasis and pcavar (reshape it into right tensors, take squareroot of sigma, matrix multiply it. 
 
-pcaVar_shape = np.asarray(bfm['shape/model/pcaVariance'], dtype=np.float32)
-pcaVar_shape = pcaVar_shape[:30]
-# print('pcaVar_shape: ', np.shape(pcaVar_shape))
+	pcaB_shape = np.asarray(bfm['shape/model/pcaBasis'], dtype=np.float32)
+	nrDim = np.shape(pcaB_shape)[1]
+	pcaB_shape = pcaB_shape.reshape(-1, 3, nrDim)
+	pcaB_shape = pcaB_shape[:, :, :30]
 
-alpha = np.random.uniform(-1, 1, (30))
-sigma_shape = alpha * np.sqrt(pcaVar_shape)
-sigma_shape = np.sqrt(pcaVar_shape)
+	pcaVar_shape = np.asarray(bfm['shape/model/pcaVariance'], dtype=np.float32)
+	pcaVar_shape = pcaVar_shape[:30]
 
-E_shape_sigma = np.matmul(pcaB_shape, sigma_shape)
-# print('E_shape_sigma:', np.shape(E_shape_sigma))
+	sigma_shape = alpha * np.sqrt(pcaVar_shape)
+	sigma_shape = np.sqrt(pcaVar_shape)
 
-# here we load expression/model/mean and convert 3N --> N x 3
+	E_shape_sigma = np.matmul(pcaB_shape, sigma_shape)
 
-mean_exp = np.asarray(bfm['expression/model/mean'], dtype=np.float32).reshape((-1, 3))
+	# here we load expression/model/mean and convert 3N --> N x 3
 
-# here we load for expression: the pcabasis and pcavar (reshape it into right tensors, take squareroot of sigma, matrix multiply it. 
+	mean_exp = np.asarray(bfm['expression/model/mean'], dtype=np.float32).reshape((-1, 3))
 
-pcaB_exp = np.asarray(bfm['expression/model/pcaBasis'], dtype=np.float32)
-nrDim = np.shape(pcaB_exp)[1]
-pcaB_exp = pcaB_exp.reshape(-1, 3, nrDim)
-# print('pcaB_exp: ', np.shape(pcaB_exp))
+	# here we load for expression: the pcabasis and pcavar (reshape it into right tensors, take squareroot of sigma, matrix multiply it. 
 
-pcaVar_exp = np.asarray(bfm['expression/model/pcaVariance'], dtype=np.float32)
+	pcaB_exp = np.asarray(bfm['expression/model/pcaBasis'], dtype=np.float32)
+	nrDim = np.shape(pcaB_exp)[1]
+	pcaB_exp = pcaB_exp.reshape(-1, 3, nrDim)
 
-size_pcaVar_shape = np.shape(pcaVar_shape)
-# print('pcaVar_exp: ', np.shape(pcaVar_exp))
+	pcaVar_exp = np.asarray(bfm['expression/model/pcaVariance'], dtype=np.float32)
 
-pcaB_exp = pcaB_exp[:, :, :20]
-pcaVar_exp = pcaVar_exp[:20]
+	pcaB_exp = pcaB_exp[:, :, :20]
+	pcaVar_exp = pcaVar_exp[:20]
 
-delta = np.random.uniform(-1, 1, (20))
-sigma_exp = delta * np.sqrt(pcaVar_exp)
-E_exp_sigma = np.matmul(pcaB_exp, sigma_exp)
-# print('E_exp_sigma: ', np.shape(E_exp_sigma))
+	sigma_exp = delta * np.sqrt(pcaVar_exp)
+	E_exp_sigma = np.matmul(pcaB_exp, sigma_exp)
 
-# combine mean_shape with Eid*[alpha*sigma]
-pointCloud2 = mean_shape + E_shape_sigma
-pointCloud3 = mean_shape + E_shape_sigma + mean_exp + E_exp_sigma
+	# combine mean_shape with Eid*[alpha*sigma]
+	pCid = mean_shape + E_shape_sigma
+	pCexp = mean_shape + E_shape_sigma + mean_exp + E_exp_sigma
 
-# this is standard code to read color/tex and triangles
-mean_tex = np.asarray(bfm['color/model/mean'], dtype=np.float32).reshape((-1, 3))
-triangles = np.asarray(bfm['shape/representer/cells'], dtype=np.int32).T
+	# this is standard code to read color/tex and triangles
+	mean_tex = np.asarray(bfm['color/model/mean'], dtype=np.float32).reshape((-1, 3))
+	triangles = np.asarray(bfm['shape/representer/cells'], dtype=np.int32).T
+
+	return pCid, pCexp, mean_tex, triangles
 
 def mesh_to_png(file_name, mesh):
     mesh = trimesh.base.Trimesh(
@@ -77,14 +68,18 @@ def mesh_to_png(file_name, mesh):
         f.write(png)
 
 if __name__ == '__main__':
-    # output: only mean_shape)
-    # mesh = Mesh(mean_shape, mean_tex, triangles)
+	# output: only mean_shape)
+	# mesh = Mesh(mean_shape, mean_tex, triangles)
 
-    # output: mean_shape + Eid*[alpha*sigma]
-    mesh = Mesh(pointCloud2, mean_tex, triangles)
-    mesh_to_png("pointCloud2.png", mesh)
+	alpha = np.random.uniform(-1, 1, (30))
+	delta = np.random.uniform(-1, 1, (20))
+	[pCidentity, pCexpression, mean_tex, triangles] = load_faces(alpha, delta)  
 
-    # output: all combined
-    mesh = Mesh(pointCloud3, mean_tex, triangles)
-    # mesh_to_png("pointCloud3.png", mesh)
+	# output: mean_shape + Eid*[alpha*sigma]
+	mesh = Mesh(pCidentity, mean_tex, triangles)
+	# mesh_to_png("pCid.png", mesh)
+
+	# output: all combined
+	mesh = Mesh(pCexpression, mean_tex, triangles)
+	mesh_to_png("pCexp.png", mesh)
 
