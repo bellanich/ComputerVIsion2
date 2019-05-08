5308da32f5775700f239c0cca56f5faf496cfdff
peterheemskerk <peter.heemskerk@planet.nl>
2019-05-08 12:20:11 +0200

    qu2

--- Assignment_3/mesh_to_png.py
+++ Assignment_3/mesh_to_png.py
@@ -7,7 +7,6 @@ import h5py
 
 from data_def import PCAModel, Mesh
 
-# Peter: added .
 bfm = h5py.File("./Data/model2017-1_face12_nomouth.h5", 'r')
 
 # here we load shape/model/mean and convert 3N --> N x 3
@@ -21,20 +20,19 @@ print(size_mean_shape)
 pcaB_shape = np.asarray(bfm['shape/model/pcaBasis'], dtype=np.float32)
 nrDim = np.shape(pcaB_shape)[1]
 pcaB_shape = pcaB_shape.reshape(-1, 3, nrDim)
-size_pcaB_shape = np.shape(pcaB_shape)
-print(size_pcaB_shape)
+pcaB_shape = pcaB_shape[:, :, :30]
+# print('pcaB_shape:', np.shape(pcaB_shape))
 
 pcaVar_shape = np.asarray(bfm['shape/model/pcaVariance'], dtype=np.float32)
-size_pcaVar_shape = np.shape(pcaVar_shape)
-print(size_pcaVar_shape)
+pcaVar_shape = pcaVar_shape[:30]
+# print('pcaVar_shape: ', np.shape(pcaVar_shape))
 
-newshape = pcaB_shape[:, :, :30]
-newVar = pcaVar_shape[:30]
-print(np.shape(newshape))
-print(np.shape(newVar))
+alpha = np.random.uniform(-1, 1, (30))
+sigma_shape = alpha * np.sqrt(pcaVar_shape)
+sigma_shape = np.sqrt(pcaVar_shape)
 
-newcombi = np.matmul(newshape, np.sqrt(newVar))
-print(np.shape(newcombi))
+E_shape_sigma = np.matmul(pcaB_shape, sigma_shape)
+# print('E_shape_sigma:', np.shape(E_shape_sigma))
 
 # here we load expression/model/mean and convert 3N --> N x 3
 
@@ -45,22 +43,24 @@ mean_exp = np.asarray(bfm['expression/model/mean'], dtype=np.float32).reshape((-
 pcaB_exp = np.asarray(bfm['expression/model/pcaBasis'], dtype=np.float32)
 nrDim = np.shape(pcaB_exp)[1]
 pcaB_exp = pcaB_exp.reshape(-1, 3, nrDim)
-print('pcaB_exp: ', np.shape(pcaB_exp))
+# print('pcaB_exp: ', np.shape(pcaB_exp))
 
 pcaVar_exp = np.asarray(bfm['expression/model/pcaVariance'], dtype=np.float32)
 
 size_pcaVar_shape = np.shape(pcaVar_shape)
-print('pcaVar_exp: ', np.shape(pcaVar_exp))
+# print('pcaVar_exp: ', np.shape(pcaVar_exp))
 
 pcaB_exp = pcaB_exp[:, :, :20]
 pcaVar_exp = pcaVar_exp[:20]
-delta = zeros(20)
-E_exp_sigma = np.matmul(pcaB_exp, np.sqrt(pcaVar_exp))
-print('E_exp_sigma: ', np.shape(E_exp_sigma))
 
-# combine mean_shape with Eid*[alpha*sigma] -TODO nog laatste twee toevoegen
-pointCloud2 = mean_shape + newcombi
-pointCloud3 = mean_shape + newcombi + mean_exp + E_exp_sigma
+delta = np.random.uniform(-1, 1, (20))
+sigma_exp = delta * np.sqrt(pcaVar_exp)
+E_exp_sigma = np.matmul(pcaB_exp, sigma_exp)
+# print('E_exp_sigma: ', np.shape(E_exp_sigma))
+
+# combine mean_shape with Eid*[alpha*sigma]
+pointCloud2 = mean_shape + E_shape_sigma
+pointCloud3 = mean_shape + E_shape_sigma + mean_exp + E_exp_sigma
 
 # this is standard code to read color/tex and triangles
 mean_tex = np.asarray(bfm['color/model/mean'], dtype=np.float32).reshape((-1, 3))
@@ -81,9 +81,10 @@ if __name__ == '__main__':
     # mesh = Mesh(mean_shape, mean_tex, triangles)
 
     # output: mean_shape + Eid*[alpha*sigma]
-    # mesh = Mesh(pointCloud2, mean_tex, triangles)
-    # mesh_to_png("pointCloud2.png", mesh)
+    mesh = Mesh(pointCloud2, mean_tex, triangles)
+    mesh_to_png("pointCloud2.png", mesh)
 
     # output: all combined
     mesh = Mesh(pointCloud3, mean_tex, triangles)
-    mesh_to_png("pointCloud3.png", mesh)
+    # mesh_to_png("pointCloud3.png", mesh)
+
