ae1da66482df00de91d6c76373460a06d460da2d
peterheemskerk <peter.heemskerk@planet.nl>
2019-05-15 13:30:16 +0200

    loop

--- Assignment_3/main.py
+++ Assignment_3/main.py
@@ -15,6 +15,7 @@ def generate_face(alpha, delta, omega, t):
     :return:
     """
     pCid, pCexp, mean_tex, triangles = load_faces(alpha, delta)
+    print('load_faces done.')
     transformed_face, landmarks = run_pinhole_camera(omega, t, pCexp, mean_tex, use_landmarks=True)
 
     return landmarks
@@ -33,8 +34,8 @@ def calculate_loss(landmarks, ground_truth_landmarks, alpha, delta, L_alpha, L_d
     TODO: expand the loss function to compare 2D vectors in stead of scalars: DONE
     """
 
-    diff = landmarks - ground_truth_landmarks
-    landmark_loss = torch.sum(torch.tensor(np.square(diff)))       
+    diff = (torch.tensor(landmarks).float() - torch.tensor(ground_truth_landmarks).float())**2
+    landmark_loss = torch.sum(torch.tensor(diff))       
 
     alpha_regularization_loss = 0
     for i in range(0,30):
@@ -44,12 +45,12 @@ def calculate_loss(landmarks, ground_truth_landmarks, alpha, delta, L_alpha, L_d
     for i in range(0,20):
         delta_regularization_loss += L_delta * delta[i] ** 2
 
-    return landmark_loss + alpha_regularization_loss + delta_regularization_loss
+    return landmark_loss + torch.tensor(alpha_regularization_loss).float() + torch.tensor(delta_regularization_loss).float()
 
 
 def run_update_loop():
     """Use tensorflow as described in the assignment
-    TODO: convert ground_truth_landmarks into XY-coordinates
+    TODO: convert ground_truth_landmarks into XY-coordinates - This is now done in face_landmark_detection.py
     """
     ground_truth_landmarks = face_landmark_detection("./Data/shape_predictor_68_face_landmarks.dat", "./Faces/")
     pass
@@ -62,25 +63,39 @@ if __name__ == "__main__":
     L_alpha = 0.5
     L_delta = 0.5
 
-    landmarks = generate_face(alpha, delta, [0, 10, 0], [0, 0, 0])
-
     ground_truth_landmarks = face_landmark_detection("./Data/shape_predictor_68_face_landmarks.dat", "./Faces/")
 
-    alpha = torch.tensor(alpha)
-    delta = torch.tensor(delta)
+    for loop in range(5):
+    # Start loop for improving Loss - this loops works but does not decrease L_fit yet .
+    # TODO - build convergence criterium
 
-    # TODO: Start loop here and termination
-    # TODO: put new landmarks generated in the loop
-    L_fit = calculate_loss(landmarks, ground_truth_landmarks, alpha, delta, L_alpha, L_delta)
-    print(L_fit)
+        # I believe we need to have alpha and delta as tensors with gradients from the beginning
+        # and ensure that the function 'generate_face' is robust for tensors 
+        # is this true ? lots of work). 
 
-    L_fit = torch.autograd.Variable(L_fit, requires_grad=True)
-    debug = 1
+        # forward pass - calculate new values
+        landmarks = generate_face(alpha, delta, [0, 10, 0], [0, 0, 0])
 
-    optimizer = torch.optim.Adam([alpha, delta], lr=0.001)
-    optimizer.zero_grad()
-    
-    L_fit.backward()
-    optimizer.step()
+        # define the Adam optimizer
+        alpha = torch.tensor(alpha, requires_grad=True)    
+        delta = torch.tensor(delta, requires_grad=True)	
+        optimizer = torch.optim.Adam([alpha, delta], lr=0.5)
+
+	# calculate loss
+        # landmarks = torch.tensor(landmarks, requires_grad=True)
+        L_fit = calculate_loss(landmarks, ground_truth_landmarks, alpha, delta, L_alpha, L_delta)
+        # L_fit = torch.autograd.Variable(L_fit, requires_grad=True)      # do not need this anymore, since function 'calculate loss' delivers tensor L_fit
+        print('loss:', L_fit)
+
+        # backward pass
+        optimizer.zero_grad()           # reset gradients
+        L_fit.backward()   		# compute the gradients 
+
+        # adjust variables
+        optimizer.step()                # adjust parameters (alpha, delta)
+
+        # to make it np arrays again, needed for forward pass - function 'generate_face'
+        alpha = alpha.detach().numpy()   
+        delta = delta.detach().numpy()
 
 
